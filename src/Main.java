dc073a759426cd65d0c61686a632bc2eb5c9ffb8
iva1111 <mvg75@mail.ru>
2021-07-19 10:04:07 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 10_000;
+        int bonus = 20;
+        int miles = ticket / bonus;
+        System.out.println(miles);
+    }
+}
